34351c57fe23353f793827d5c5f634586793a568
sammyfischer <sammyfischer50@gmail.com>
2024-09-13 22:30:37 -0400

    created profile page and set up vuetify

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>My Profile</v-card-title>
+        <v-card-text>
+            <v-row>
+                <v-col cols="auto">
+                    <v-avatar color="purple">FL</v-avatar>
+                </v-col>
+                <v-col cols="auto">
+                    <h3>Firstname Lastname</h3>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
+</template>
