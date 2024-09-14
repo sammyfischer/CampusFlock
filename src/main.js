6eeee6b18bd0cccafe972bdd924bfb9642468379
Musa Haji <mqhaji@vt.edu>
2024-09-14 13:31:10 -0400

    add router, add bottom navbar, add material design web font, add home page

--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import router from './router'; // Import the router
+import '@mdi/font/css/materialdesignicons.css';
 
+
+// Create Vuetify instance
 const vuetify = createVuetify({
   components,
   directives,
 });
 
 const app = createApp(App);
-app.use(vuetify);
-app.mount('#app');
-
+app.use(vuetify);  // Use Vuetify
+app.use(router);    // Use the router
+app.mount('#app');  // Mount the app
